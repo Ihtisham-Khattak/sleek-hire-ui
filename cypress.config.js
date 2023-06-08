87a8df6362c18e510bd0845f35a75c09f35650a0
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-06-08 16:52:30 +0500

    All api testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://us-central1-closing-x.cloudfunctions.net/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/5-closingx/api_testing.cy.js
@@ -0,0 +1,53 @@
+///<reference types="Cypress" />
+
+describe("Products api", () => {
+  // Get Request
+  it("Closing-X Get", () => {
+    cy.request(
+      "GET",
+      "https://us-central1-closing-x.cloudfunctions.net/getValues"
+    )
+      .its("status")
+      .should("equal", 200);
+  });
+
+  // Post Request
+  it("Closing-X Post", () => {
+    cy.request({
+      method: "POST",
+      url: "https://us-central1-closing-x.cloudfunctions.net/getWinners",
+      body: {
+        winningTargetNumber: "13",
+      },
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+
+  // Put Request
+  it("Closing-X Put", () => {
+    cy.request({
+      method: "PUT",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+      body: {
+        userId: 1,
+        id: 1,
+        title:
+          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "This is home ",
+      },
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+
+  //Delete Request
+  it("Closing-X Delete", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+});
